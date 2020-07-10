3218f3277a0cae554b1960a6a785f120577a92f7
Kazon Wilson <kazonwilson@gmail.com>
2020-07-09 21:17:12 -0700

    Add thread view to users,

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,62 @@
 import Head from "next/head";
 
-import { Grid, Container, Divider, Text, Page } from "@zeit-ui/react";
+import {
+  Grid,
+  Container,
+  Divider,
+  Text,
+  Page,
+  Spacer,
+  Spinner,
+  Col,
+  useToasts,
+} from "@zeit-ui/react";
 
 import ThreadCardGroup from "../components/ThreadCardGroup";
 import SearchBox from "../components/SearchBox";
 import Sticky from "react-stickynode";
+import { useState } from "react";
+import { useQuery } from "graphql-hooks";
+import { allThreadsQuery } from "../queries/allThreadsQuery";
+
+const updateData = (prevData, data) => {
+  return {
+    threads: {
+      items: [...prevData.threads.items, ...data.threads.items],
+      count: data.threads.count,
+    },
+  };
+};
 
 export default function Home() {
+  const limit = 15;
+
+  const [offsetCount, setOffsetCount] = useState(0);
+  const [toasts, setToast] = useToasts();
+
+  const { loading, error, data } = useQuery(allThreadsQuery, {
+    variables: { offset: offsetCount, limit },
+    updateData,
+  });
+
+  if (error) {
+    setToast({
+      text: "An error has occured while fetching data",
+      type: "error",
+    });
+  }
+
+  if (loading && !data)
+    return (
+      <Container style={{ padding: 20, margin: "0 auto" }} justify="center">
+        <Spacer x={8} />
+        <Spinner size="large" />
+        <Spacer x={8} />
+      </Container>
+    );
+
+  const hasMoreThreads = data.threads.items.length < data.threads.count;
+
   return (
     <Page>
       <Container style={{ margin: "0 auto" }}>
@@ -45,7 +95,18 @@ export default function Home() {
             <Grid xs={24}>
               <Text h2>Recent</Text>
             </Grid>
-            <ThreadCardGroup />
+            <ThreadCardGroup
+              data={data}
+              hasMoreThreads={hasMoreThreads}
+              setOffsetCount={setOffsetCount}
+              offsetCount={offsetCount}
+              limit={limit}
+            />
+            {loading && (
+              <Col align="center">
+                <Spinner size="large" />
+              </Col>
+            )}
           </Grid.Container>
         </Page.Content>
       </Container>
